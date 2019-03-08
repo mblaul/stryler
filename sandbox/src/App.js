3fb3f3ecd0f4190b2e32f1f51d7ec5de006773a4
mblaul <mattblaul@gmail.com>
2019-03-07 21:57:34 -0500

    Added a splitOn method for simple splits, refined how elements are wrapped

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -5,21 +5,29 @@ import Stryler from './utils/stryler';
 
 class App extends Component {
 	render() {
-		const sampleString = 'Lorem ipsum';
+		const sampleString = `Lorem Ipsum`;
 
 		return (
 			<div className="App">
 				<header className="App-header">
-					<p>
+					<div>
 						<Stryler
 							string={sampleString}
 							splitAt={[ 3, 6 ]}
-							splitOn={' '}
 							WrapperElements={[ 'span', 'div', 'span' ]}
 							classNames={[ 'App-link', 'App-text', 'App-link' ]}
-							styles={[ { fontSize: 36 }, { transform: 'rotate(90deg)' } ]}
+							styles={[ { fontSize: 36 }, { transform: 'rotate(-45deg)' } ]}
 						/>
-					</p>
+					</div>
+					<div style={{ paddingTop: 40 }}>
+						<Stryler
+							string={sampleString}
+							splitOn={' '}
+							WrapperElements={[ 'span', 'div' ]}
+							classNames={[ 'App-link', 'App-text' ]}
+							styles={[ { fontSize: 36 }, { transform: 'rotate(-15deg)' } ]}
+						/>
+					</div>
 				</header>
 			</div>
 		);
--- sandbox/src/utils/stryler/index.js
+++ sandbox/src/utils/stryler/index.js
@@ -1,24 +1,12 @@
 import React from 'react';
 
-function index({ string, splitAt, splitOn, WrapperElements = [ 'span' ], classNames = [], styles = [] }) {
+function createElements({ stringSegments, classNames, styles, WrapperElements, joinOn }) {
 	let styledStrings = [];
-	let splitString = [];
 
-	if (splitAt) {
-		splitAt.forEach((split, index) => {
-			const lastIndex = index === 0 ? 0 : splitAt[index - 1];
-			const currentIndex = splitAt[index];
-			splitString.push(string.substring(lastIndex, currentIndex));
-		});
-		splitString.push(string.substring(splitAt[splitAt.length - 1]));
-	}
-
-	if (splitOn) {
-		//TODO: Add a splitOn handler for simple strings
-	}
-
-	splitString.forEach((string, index) => {
+	stringSegments.forEach((string, index) => {
 		let WrapperElement = WrapperElements[index];
+
+		if (joinOn && index !== stringSegments.length) string += joinOn;
 		let styledElement = (
 			<WrapperElement className={classNames[index]} style={styles[index]} key={index}>
 				{string}
@@ -27,6 +15,32 @@ function index({ string, splitAt, splitOn, WrapperElements = [ 'span' ], classNa
 
 		styledStrings.push(styledElement);
 	});
+	return styledStrings;
+}
+
+function index({ string, splitAt, splitOn, WrapperElements = [ 'span' ], classNames = [], styles = [] }) {
+	let styledStrings = [];
+	let stringSegments = [];
+
+	//TODO: Add splitEvery to allow more control
+	//TODO: Make a styles object composed of Wrappers, classNames and styles
+	//TODO: Allow repeating of styles and classNames over a limited string
+
+	if (splitAt) {
+		splitAt.forEach((split, index) => {
+			const lastIndex = index === 0 ? 0 : splitAt[index - 1];
+			const currentIndex = splitAt[index];
+			stringSegments.push(string.substring(lastIndex, currentIndex));
+		});
+		stringSegments.push(string.substring(splitAt[splitAt.length - 1]));
+
+		styledStrings = createElements({ stringSegments, classNames, styles, WrapperElements });
+	}
+
+	if (splitOn) {
+		stringSegments = string.split(splitOn);
+		styledStrings = createElements({ stringSegments, classNames, styles, WrapperElements, joinOn: splitOn });
+	}
 
 	return <React.Fragment>{styledStrings.map((styledString) => styledString)}</React.Fragment>;
 }

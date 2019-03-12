22d28f83aaed7fb808f191813d7582f8586bea10
mblaul <mattblaul@gmail.com>
2019-03-11 21:41:24 -0400

    Deleting some unnecessary files and working on frontend for sandbox experience

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,37 +1,60 @@
 import React, { Component } from 'react';
-import './App.css';
 
 import Stryler from './utils/stryler';
 
 class App extends Component {
-	render() {
-		const sampleString = `Lorem Ipsum`;
+  constructor(props) {
+    super(props);
 
-		return (
-			<div className="App">
-				<header className="App-header">
-					<div>
-						<Stryler
-							string={sampleString}
-							splitAt={[ 3, 6 ]}
-							WrapperElements={[ 'span', 'div', 'span' ]}
-							classNames={[ 'App-link', 'App-text', 'App-link' ]}
-							styles={[ { fontSize: 36 }, { transform: 'rotate(-45deg)' } ]}
-						/>
-					</div>
-					<div style={{ paddingTop: 40 }}>
-						<Stryler
-							string={sampleString}
-							splitOn={' '}
-							WrapperElements={[ 'span', 'div' ]}
-							classNames={[ 'App-link', 'App-text' ]}
-							styles={[ { fontSize: 36 }, { transform: 'rotate(-15deg)' } ]}
-						/>
-					</div>
-				</header>
-			</div>
-		);
-	}
+    this.state = {
+      sampleString: '',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const sampleString = `Lorem Ipsum`;
+
+    return (
+      <div className="container-fluid">
+        <div className="col-md-4 col-xs-12 mx-auto">
+          <div className="display-3 py-2 text-center">Stryler</div>
+          <h4 className="pb-5 text-center">A tool for styling simple strings</h4>
+          <div class="form-group pb-5">
+            <label for="sampleString">Test a cool string</label>
+            <input
+              type="text"
+              class="form-control"
+              id="sampleString"
+              aria-describedby="sampleStringHelp"
+              placeholder="Lorem Ipsum"
+            />
+          </div>
+          <div className={'row'}>
+            <Stryler
+              string={sampleString}
+              splitAt={[ 3, 6 ]}
+              WrapperElements={[ 'span', 'div', 'span' ]}
+              classNames={[ 'alert-warning', 'alert-success', 'alert-danger' ]}
+              styles={[ { fontSize: 36 }, { transform: 'rotate(-45deg)' }, { lineHeight: 1 } ]}
+            />
+          </div>
+          <div className={'row text-center'}>
+            <Stryler
+              string={sampleString}
+              splitOn={' '}
+              WrapperElements={[ 'span', 'div' ]}
+              classNames={[ 'App-link', 'App-text' ]}
+              styles={[ { fontSize: 36 }, { transform: 'rotate(-15deg)' } ]}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- sandbox/src/utils/stryler/index.js
+++ sandbox/src/utils/stryler/index.js
@@ -1,48 +1,48 @@
 import React from 'react';
 
 function createElements({ stringSegments, classNames, styles, WrapperElements, joinOn }) {
-	let styledStrings = [];
+  let styledStrings = [];
 
-	stringSegments.forEach((string, index) => {
-		let WrapperElement = WrapperElements[index];
+  stringSegments.forEach((string, index) => {
+    let WrapperElement = WrapperElements[index];
 
-		if (joinOn && index !== stringSegments.length) string += joinOn;
-		let styledElement = (
-			<WrapperElement className={classNames[index]} style={styles[index]} key={index}>
-				{string}
-			</WrapperElement>
-		);
+    if (joinOn && index !== stringSegments.length) string += joinOn;
+    let styledElement = (
+      <WrapperElement className={classNames[index]} style={styles[index]} key={index}>
+        {string}
+      </WrapperElement>
+    );
 
-		styledStrings.push(styledElement);
-	});
-	return styledStrings;
+    styledStrings.push(styledElement);
+  });
+  return styledStrings;
 }
 
 function index({ string, splitAt, splitOn, WrapperElements = [ 'span' ], classNames = [], styles = [] }) {
-	let styledStrings = [];
-	let stringSegments = [];
+  let styledStrings = [];
+  let stringSegments = [];
 
-	//TODO: Add splitEvery to allow more control
-	//TODO: Make a styles object composed of Wrappers, classNames and styles
-	//TODO: Allow repeating of styles and classNames over a limited string
+  //TODO: Add splitEvery to allow more control
+  //TODO: Make a styles object composed of Wrappers, classNames and styles
+  //TODO: Allow repeating of styles and classNames over a limited string
 
-	if (splitAt) {
-		splitAt.forEach((split, index) => {
-			const lastIndex = index === 0 ? 0 : splitAt[index - 1];
-			const currentIndex = splitAt[index];
-			stringSegments.push(string.substring(lastIndex, currentIndex));
-		});
-		stringSegments.push(string.substring(splitAt[splitAt.length - 1]));
+  if (splitAt) {
+    splitAt.forEach((split, index) => {
+      const lastIndex = index === 0 ? 0 : splitAt[index - 1];
+      const currentIndex = splitAt[index];
+      stringSegments.push(string.substring(lastIndex, currentIndex));
+    });
+    stringSegments.push(string.substring(splitAt[splitAt.length - 1]));
 
-		styledStrings = createElements({ stringSegments, classNames, styles, WrapperElements });
-	}
+    styledStrings = createElements({ stringSegments, classNames, styles, WrapperElements });
+  }
 
-	if (splitOn) {
-		stringSegments = string.split(splitOn);
-		styledStrings = createElements({ stringSegments, classNames, styles, WrapperElements, joinOn: splitOn });
-	}
+  if (splitOn) {
+    stringSegments = string.split(splitOn);
+    styledStrings = createElements({ stringSegments, classNames, styles, WrapperElements, joinOn: splitOn });
+  }
 
-	return <React.Fragment>{styledStrings.map((styledString) => styledString)}</React.Fragment>;
+  return <React.Fragment>{styledStrings.map((styledString) => styledString)}</React.Fragment>;
 }
 
 export default index;
